0618e8dff4091850938cc50603b991d28710d3f5
Eric Beragg <eric.beragg@gmail.com>
2017-03-16 22:32:55 +0100

    Updated the index file with the new components

--- packages/react-blades/index.js
+++ packages/react-blades/index.js
@@ -2,10 +2,18 @@ var BladeManager = require('./dist/components/BladeManager').default;
 var BladeProvider = require('./dist/components/BladeProvider').default;
 var BladePresenter = require('./dist/components/BladePresenter').default;
 var Blade = require('./dist/components/Blade').default;
+var BladeHeader = require('./dist/components/BladeHeader').default;
+var BladeToolbar = require('./dist/components/BladeToolbar').default;
+var BladeToolbarButton = require('./dist/components/BladeToolbarButton').default;
+var BladeContent = require('./dist/components/BladeContent').default;
 
 module.exports = {
   BladeManager: BladeManager,
   BladeProvider: BladeProvider,
   BladePresenter: BladePresenter,
-  Blade: Blade
+  Blade: Blade,
+  BladeHeader: BladeHeader,
+  BladeToolbar: BladeToolbar,
+  BladeToolbarButton: BladeToolbarButton,
+  BladeContent: BladeContent,
 };
