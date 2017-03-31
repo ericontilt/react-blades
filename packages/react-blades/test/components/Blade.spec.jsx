03202cd6721f75cd2b1f5577c5698d0986d66fac
Eric Beragg <eric.beragg@gmail.com>
2017-03-31 09:58:13 +0200

    Revert "Revert "Adding tests""
    This reverts commit 50055b583b6b36f10e8b646e91d6df10686196d2.

--- packages/react-blades/test/components/Blade.spec.jsx
+++ packages/react-blades/test/components/Blade.spec.jsx
@@ -5,26 +5,23 @@ import Blade from '../../src/js/components/Blade';
 describe('Blade', () => {
   describe('#render', () => {
     it('is .Blade', () => {
-      const visibleBlades = {
-        blades: {
-        },
-      };
-      const wrapper = shallow(<Blade />, {
-        context: visibleBlades,
-      });
+      const wrapper = shallow(<Blade />);
       expect(wrapper.is('.Blade')).toEqual(true);
     });
 
     it('applies className', () => {
-      const visibleBlades = {
-        blades: {
-        },
-      };
-      const wrapper = shallow(<Blade className="Test" />, {
-        context: visibleBlades,
-      });
+      const wrapper = shallow(<Blade className="Test" />);
       expect(wrapper.is('.Blade')).toEqual(true);
       expect(wrapper.is('.Test')).toEqual(true);
+    });
+
+    it('renders children', () => {
+      const wrapper = shallow(
+        <Blade className="Test">
+          <div className="ChildComponent" />
+        </Blade>,
+      );
+      expect(wrapper.children().length).toEqual(1);
     });
   });
 });
